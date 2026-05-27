58cc386694ee98aa27481e4ed2c6b4cee8833770
turjo4620 <sarkerturjo2022@gmail.com>
2026-05-27 10:01:46 +0600

    added

--- First.py
+++ First.py
@@ -1,2 +1,10 @@
-x = 10
-print(x)
+spam_amount = 0
+print(spam_amount)
+
+spam_amount += 4
+
+if(spam_amount > 0) :
+    print("But I don't want ANY spam!")
+
+viking_song = "Spam " * spam_amount
+print(viking_song)
